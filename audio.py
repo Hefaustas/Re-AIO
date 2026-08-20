4e207f802c6970dc807b12caf40e4e83310e61cc
Hefaustas <hefaistaskulba@gmail.com>
2026-08-20 23:02:28 +0300

    Functional audio
    wav/mp3/ogg vorbis playback is now fully functional, however opus support is dead, pybass3 doesnt support loading plugins as it seems.

--- audio.py
+++ audio.py
@@ -1,7 +1,7 @@
 # platform-dependent audio handling using BASS, basically stolen from the old AIO
 # modified to use pybass3 for 64-bit compatibility
 
-# TODO: I think the current implemention of audio is probably broken and will need to be redone from the ground up.
+# TODO: pybass3 doesn't support plugin loading, so OPUS support is dead currently. Need to figure out solution. Probably ctypes?
 import platform, ctypes, os
 from pybass_constants import *
 from configparser import ConfigParser
@@ -31,6 +31,15 @@ Returns string with dll name if it's missing, empty if all DLLs are in place
     if not two: return opus
     return ""
 
+
+def _pybass_helpers():
+    import pybass3.bass_module as bass_module
+    from pybass3.bass_module import Bass, func_type
+    from pybass3.bass_channel import BassChannel
+    from pybass3.bass_stream import BassStream
+    return bass_module, Bass, BassChannel, BassStream, func_type
+
+
 def init(freq=48000):
     """
 Initialize BASS and the opus plugin
@@ -48,8 +57,14 @@ Initialize BASS and the opus plugin
     config.read("re-aio.ini")
 
     if use_pybass3:
-        dll.bass_module.BASS_Init(config.getint("Audio", "device_index", fallback=-1), freq, 0, 0, 0)
-        #dll.bass_stream.BASS_PluginLoad(os.path.abspath(opus), 0)
+        _, Bass, _, _, func_type = _pybass_helpers()
+        Bass.Init(config.getint("Audio", "device_index", fallback=-1), freq, 0, 0, 0)
+        try:
+            bass_module, _, _, _, func_type = _pybass_helpers()
+            BASS_PluginLoad = func_type(ctypes.c_ulong, ctypes.c_char_p, ctypes.c_ulong)(('BASS_PluginLoad', bass_module.bass_module))
+            BASS_PluginLoad(os.path.abspath(opus).encode('utf-8'), 0)
+        except Exception:
+            pass
     else:    
         dll.BASS_Init(config.getint("Audio", "device_index", fallback=-1), freq, 0, 0, 0)
         dll.BASS_PluginLoad(os.path.abspath(opus), 0)
@@ -59,24 +74,35 @@ def free():
     """
 Free BASS
     """
+    if use_pybass3:
+        _, Bass, _, _, _ = _pybass_helpers()
+        Bass.Free()
+        return
     dll.BASS_Free()
 
 def getcurrdevice():
+    if use_pybass3:
+        _, Bass, _, _, _ = _pybass_helpers()
+        return Bass.GetCurrentDeviceID()
     return dll.BASS_GetDevice()
 
 def getdevices():
     devices = []
     i = 0
-    info = BASS_DEVICEINFO()
 
     if use_pybass3:
-        bass_api = dll.bass_module
-        while bass_api.BASS_GetDeviceInfo(i, ctypes.byref(info)):
+        _, Bass, _, _, _ = _pybass_helpers()
+        while True:
+            try:
+                info = Bass.GetDeviceInfo(i)
+            except Exception:
+                break
             name = info.name.decode("utf-8", errors="replace") if isinstance(info.name, bytes) else str(info.name)
             devices.append((i, name))
             i += 1
         return devices
 
+    info = BASS_DEVICEINFO()
     if use_ctypes:
         while dll.BASS_GetDeviceInfo(i, ctypes.byref(info)):
             name = info.name.decode("utf-8", errors="replace") if isinstance(info.name, bytes) else str(info.name)
@@ -95,12 +121,22 @@ def loadhandle(mem, file, offset=0, length=0, flags=0):
     """
 Load a BASS stream handle
     """
+    if use_pybass3:
+        from pybass3.bass_stream import BASS_StreamCreateFile
+        if isinstance(file, str):
+            file = file.encode('utf-8')
+        file_ptr = ctypes.c_char_p(file)
+        return BASS_StreamCreateFile(mem, file_ptr, QWORD(offset), QWORD(length), flags)
     return dll.BASS_StreamCreateFile(mem, file, QWORD(offset), QWORD(length), flags)
 
 def loadURLhandle(url, offset=0, flags=0, proc=DOWNLOADPROC(), user=0):
     """
 Load a BASS stream handle from a URL
     """
+    if use_pybass3:
+        bass_module, _, _, _, func_type = _pybass_helpers()
+        BASS_StreamCreateURL = func_type(ctypes.c_ulong, ctypes.c_char_p, ctypes.c_ulong, ctypes.c_ulong, ctypes.c_void_p)(('BASS_StreamCreateURL', bass_module.bass_module))
+        return BASS_StreamCreateURL(url.encode('utf-8') if isinstance(url, str) else url, offset, flags, proc, user)
     return dll.BASS_StreamCreateURL(url, offset, flags, proc, user)
 
 def loadmusic(mem, file, offset=0, length=0, flags=0):
@@ -113,36 +149,56 @@ def freemusic(handle):
     """
 Free a MOD music handle from memory
     """
+    if use_pybass3:
+        bass_module, _, _, _, func_type = _pybass_helpers()
+        BASS_MusicFree = func_type(ctypes.c_bool, ctypes.c_ulong)(('BASS_MusicFree', bass_module.bass_module))
+        return BASS_MusicFree(handle)
     return dll.BASS_MusicFree(handle)
 
 def freehandle(handle):
     """
 Free a handle
     """
+    if use_pybass3:
+        from pybass3.bass_stream import BassStream
+        return BassStream.Free(handle)
     return dll.BASS_StreamFree(handle)
 
 def playhandle(handle, restart):
     """
 Play a handle
     """
+    if use_pybass3:
+        from pybass3.bass_channel import BassChannel
+        return BassChannel.Play(handle, restart)
     return dll.BASS_ChannelPlay(handle, restart)
 
 def stophandle(handle):
     """
 Stop a handle
     """
+    if use_pybass3:
+        from pybass3.bass_channel import BassChannel
+        return BassChannel.Stop(handle)
     return dll.BASS_ChannelStop(handle)
 
 def handleisactive(handle):
     """
 Get handle playback status
     """
+    if use_pybass3:
+        from pybass3.bass_channel import BassChannel
+        return BassChannel.IsActive(handle)
     return dll.BASS_ChannelIsActive(handle)
 
 def sethandleattr(handle, attr, value):
     """
 Set an attribute for a handle
     """
+    if use_pybass3:
+        bass_module, _, _, _, func_type = _pybass_helpers()
+        BASS_ChannelSetAttribute = func_type(ctypes.c_bool, ctypes.c_ulong, ctypes.c_ulong, ctypes.c_float)(('BASS_ChannelSetAttribute', bass_module.bass_module))
+        return BASS_ChannelSetAttribute(handle, attr, ctypes.c_float(value) if isinstance(value, float) else value)
     if use_ctypes and type(value) == float: 
         value = ctypes.c_float(value)
     return dll.BASS_ChannelSetAttribute(handle, attr, value)
